0725c84fc9f105e2bbe49b574bc3473d18edb9a5
Alexander Fache <alexander.fache@gmail.com>
2020-03-19 13:34:48 -0400

    updated check and push scripts

--- check.sh
+++ check.sh
@@ -4,7 +4,9 @@
 git status
 
 find ./* -size +50M | cat >> .check
+find ./* -size +50M | cat >> .gitignore
 
 #check the .check file and ensure that all files shown are .npy or .xlsx
-#these files are too large (>50Mb) for GitHub and need to be ignored
+#these files are too large (>50M) for GitHub and need to be ignored
 #if there are other files then they should probably be added to .gitignore
+#https://github.com/sr320/course-fish546-2015/issues/43
